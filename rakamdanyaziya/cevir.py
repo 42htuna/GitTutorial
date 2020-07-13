01b08811be4e4254a7b6ad8dcfbd6c44c35f42c6
42htuna <42htuna@gmail.com>
2020-07-14 01:14:10 +0300

    Add files via upload

---
+++ rakamdanyaziya/cevir.py
@@ -0,0 +1,13 @@
+import cevirici
+
+rakam = input("Rakam giriniz : ")
+
+if rakam.find(",") != -1:
+    a = rakam.split(',')
+else:
+    a = rakam.split('.')
+    
+lira = cevirici.Cevirici(a[0][:36]).yaz
+kurus = cevirici.Cevirici(a[1][:2]).yaz
+
+print("Yalnız : %s TL %s KR'dir." %(lira, kurus))
