e849e8d89db27969de0c7c65662441e7c6e27691
Hakan Tuna <64739024+42htuna@users.noreply.github.com>
2020-07-27 15:26:26 +0300

    Add files via upload

---
+++ cevir.py
@@ -0,0 +1,21 @@
+import cevirici
+
+rakam = input("Rakamı giriniz : ")
+   
+if rakam.find(",") != -1:
+    a = rakam.split(',')
+
+elif rakam.find(".") != -1:
+    a = rakam.split('.')
+    
+else:
+    rakam = rakam+".00"
+    a = rakam.split('.')
+
+if len(a[1])%2 == 1:
+    a[1] = a[1]+"0"
+
+lira = cevirici.Cevirici(a[0][:36]).yaz
+kurus = cevirici.Cevirici(a[1][:2]).yaz
+
+print("Yalnız : %s TL %s KR'dir." %(lira, kurus))
